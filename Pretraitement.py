08cd07aa7e8df0f7cc39c6fc90579d8470e2bd38
Vianney-Premier Nanda <nandanv@3il.fr>
2022-06-04 21:55:34 +0200

    Formatage pour le traitement permis de conduire

--- Pretraitement.py
+++ Pretraitement.py
@@ -17,9 +17,6 @@ import matplotlib.pyplot as plt
 # otsu et après binairisation
 
 
-# Read Image Recto
-imageRecto = cv2.imread('Images/permis-recto.jpg')
-imageVerso = cv2.imread('Images/permis-verso.jpg')
 
 # Display Image
 # cv2.imshow('imageRecto', imageRecto)
@@ -30,71 +27,116 @@ imageVerso = cv2.imread('Images/permis-verso.jpg')
 # cv2.destroyAllWindows()
 
 """
-Pré-traitement classique
+Permis de conduire
 """
+chemin_recto = 'Images/permis-recto.jpg'
+chemin_verso = 'Images/permis-verso.jpg'
+def extractPermis(chemin_recto,chemin_verso):
+    # Read Image Recto
+    imageRecto = cv2.imread(chemin_recto)
+    imageVerso = cv2.imread(chemin_verso)
 
-# Echalonage
-imageRecto = cv2.resize(imageRecto, (506, 316), fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
-imageVerso = cv2.resize(imageVerso, (506, 316), fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
-"""
-cv2.imshow('imageVerso', imageVerso)
-img = cv2.cvtColor(imageVerso, cv2.COLOR_BGR2GRAY)
-otsu_threshold, image_result = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
-cv2.imshow('image Thresh_OTSU', image_result)
-cv2.imshow('image I', img)
-print(easyocr.Reader(['fr'], gpu=True).readtext(image_result, detail=0))
-"""
-# Recadrage de la zone de texte
-yRecto = 65
-xRecto = 164
-hRecto = 80
-wRecto = 310
-recto = []
-"""
-#65:80,163:310 img
-#85:100,163:310 img
-#105:120,163:310
-#125:140,163;310 img
-#145:160 img
-#160:175 img
-champRecto = imageRecto[yRecto:hRecto, xRecto:wRecto]
-# cv2.waitKey(0)
-cv2.imshow('Image', champRecto)
-cv2.waitKey(0)
-img = cv2.cvtColor(champRecto, cv2.COLOR_BGR2GRAY)
-otsu_threshold, image_result = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
-ret, th = cv2.threshold(img, (otsu_threshold - 69), 255, cv2.THRESH_BINARY)
-imginv = cv2.bitwise_not(th)
-taille = (2, 2)
-kernel = np.ones(taille, np.uint8)
-dilateinv = cv2.dilate(imginv, kernel, iterations=1)
-cv2.imshow('Image', image_result)
-cv2.waitKey(0)
-print(easyocr.Reader(['fr'], gpu=True).readtext(img, detail=0))
-"""
-"""
-Extraction recto
-"""
-print("Recto")
-for i in range(5):
-    champRecto= imageRecto[yRecto:hRecto, xRecto:wRecto]
-    img = cv2.cvtColor(champRecto, cv2.COLOR_BGR2GRAY)
-    recto.append(easyocr.Reader(['fr'], gpu=True).readtext(champRecto, detail=0))
+    """
+    Pré-traitement classique
+    """
+    # Echalonage
+    imageRecto = cv2.resize(imageRecto, (506, 316), fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    imageVerso = cv2.resize(imageVerso, (506, 316), fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+
+    # Recadrage de la zone de texte
+    yRecto = 65
+    xRecto = 164
+    hRecto = 80
+    wRecto = 310
+    recto = []
+    result = []
+
+    """
+    Extraction recto
+    """
+    print("Recto")
+    for i in range(5):
+        champRecto= imageRecto[yRecto:hRecto, xRecto:wRecto]
+        img = cv2.cvtColor(champRecto, cv2.COLOR_BGR2GRAY)
+        recto.append(easyocr.Reader(['fr'], gpu=True).readtext(img, detail=0))
+        time.sleep(0.2)
+        yRecto = yRecto + 20
+        hRecto = hRecto + 20
+
+    img = cv2.cvtColor(imageRecto[160:175, xRecto:wRecto], cv2.COLOR_BGR2GRAY)
+    recto.append(easyocr.Reader(['fr'], gpu=True).readtext(img, detail=0))
     time.sleep(0.2)
-    yRecto = yRecto + 20
-    hRecto = hRecto + 20
+    print(recto)
 
-img = cv2.cvtColor(imageRecto[160:175, xRecto:wRecto], cv2.COLOR_BGR2GRAY)
-recto.append(easyocr.Reader(['fr'], gpu=True).readtext(champRecto, detail=0))
-time.sleep(0.2)
-print(recto)
+    """
+    Traitement du recto
+    """
+    for elt in recto:
+        for i in elt:
+            lt = i.split(' ')
+            for l in lt:
+                if l.__contains__("("):
+                    result.append(l[1:])
+                elif l.__contains__(")"):
+                    result.append(l[:3])
+                elif not (l.__contains__("4c")):
+                    result.append(l)
+    print(result)
+
+    yVerso = 30
+    xVerso = 223
+    hVerso = 43
+    wVerso = 338
+    verso = []
+    categories = ['AM','A1','A2','A','B1','B','C1','C','D1','D','BE','C1E','CE','D1E','DE']
+    """
+    Extraction verso
+    """
+    print("Verso")
+    for i in range(15):
+        champVerso = imageVerso[yVerso:hVerso, xVerso:wVerso]
+        img = cv2.cvtColor(champVerso, cv2.COLOR_BGR2GRAY)
+        verso.append(easyocr.Reader(['fr'], gpu=True).readtext(champVerso, detail=0))
+        time.sleep(0.2)
+        yVerso = yVerso + 15
+        hVerso = hVerso + 15
+
+    for i in range(15):
+        if verso[i]:
+            result.append(categories[i])
+    print(result)
 
 """
-Traitement du recto
+    cv2.imshow('imageVerso', imageVerso)
+    img = cv2.cvtColor(imageVerso, cv2.COLOR_BGR2GRAY)
+    otsu_threshold, image_result = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+    cv2.imshow('image Thresh_OTSU', image_result)
+    cv2.imshow('image I', img)
+    print(easyocr.Reader(['fr'], gpu=True).readtext(image_result, detail=0))
 """
 
-lt = [i.split(' ', 1) for i in recto[2]]
-print(lt)
+"""
+    #65:80,163:310 img
+    #85:100,163:310 img
+    #105:120,163:310
+    #125:140,163;310 img
+    #145:160 img
+    #160:175 img
+    champRecto = imageRecto[yRecto:hRecto, xRecto:wRecto]
+    # cv2.waitKey(0)
+    cv2.imshow('Image', champRecto)
+    cv2.waitKey(0)
+    img = cv2.cvtColor(champRecto, cv2.COLOR_BGR2GRAY)
+    otsu_threshold, image_result = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+    ret, th = cv2.threshold(img, (otsu_threshold - 69), 255, cv2.THRESH_BINARY)
+    imginv = cv2.bitwise_not(th)
+    taille = (2, 2)
+    kernel = np.ones(taille, np.uint8)
+    dilateinv = cv2.dilate(imginv, kernel, iterations=1)
+    cv2.imshow('Image', image_result)
+    cv2.waitKey(0)
+    print(easyocr.Reader(['fr'], gpu=True).readtext(img, detail=0))
+    """
 
 """
 for i in range(6):
@@ -119,29 +161,6 @@ for i in range(6):
 
 print(recto)
 """
-
-yVerso = 30
-xVerso = 223
-hVerso = 43
-wVerso = 338
-deltaY = 29
-verso = []
-categories = ['AM','A1','A2','A','B1','B','C1','C','D1','D','BE','C1E','CE','D1E','DE']
-"""
-Extraction verso
-"""
-print("Verso")
-for i in range(15):
-    champVerso = imageVerso[yVerso:hVerso, xVerso:wVerso]
-    img = cv2.cvtColor(champVerso, cv2.COLOR_BGR2GRAY)
-    verso.append(easyocr.Reader(['fr'], gpu=True).readtext(champVerso, detail=0))
-    time.sleep(0.2)
-    yVerso = yVerso + 15
-    hVerso = hVerso + 15
-
-for i in range(15):
-    if verso[i]:
-        print(categories[i])
 
 """
 nom = imageRecto[yRecto:yRecto + h, x:x + w]
